b21555f8f3ef496ca9ab451107602f99fc8ee056
Gabriel Hicks <gabrielhicks@mail.com>
2020-08-10 21:25:46 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -61,9 +61,13 @@ end
 
 def runner
   welcome
-  initial_round
-  prompt_user
-  hit?(initial_round)
+  card_total = initial_round
+  until card_total > 21
+    card_total = hit?(card_total)
+    
+    display_card_total(card_total)
+  end
   
+end_game(card_total)
 end
     
