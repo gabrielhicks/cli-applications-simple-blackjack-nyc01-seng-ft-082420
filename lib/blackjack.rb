71494d53261a3567d1cadcd948c0bf46144ba9b4
Gabriel Hicks <gabrielhicks@mail.com>
2020-08-10 20:33:47 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -33,9 +33,9 @@ end
 def hit?(card_total)
   prompt_user
   get_user_input
-  if get_user_input == 'h'
-    card_total + draw_card
-  end
+  # if get_user_input == 'h'
+  #   card_total + draw_card
+  # end
   card_total
 end
 
