ee07f86659d3eb793f9e04a0215da061a07a57a5
Gabriel Hicks <gabrielhicks@mail.com>
2020-08-10 20:29:55 +0000

    Automatically backed up by Learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -34,10 +34,10 @@ def hit?(number)
   prompt_user
   get_user_input
   if get_user_input == 's'
-    return initial_round
+    initial_round
   end 
   if get_user_input == 'h'
-    return initial_round + deal_card
+     initial_round + deal_card
   end
   invalid_command
   prompt_user
